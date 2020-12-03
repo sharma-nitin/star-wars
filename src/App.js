2153bd8765320c580d0d4a1c3210bc8a57193c0a
Nitin Sharma <nitin.g.sharma@capgemini.com>
2020-12-03 15:51:04 +0530

    Hah routing implemented

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
